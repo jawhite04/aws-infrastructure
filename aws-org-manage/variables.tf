7195548b876633af049b05029ce183fb6977705e
John White <jawhite04@gmail.com>
2024-06-07 13:07:50 -0400

    Move emails to tfvars

---
+++ aws-org-manage/variables.tf
@@ -0,0 +1,14 @@
+variable "aws_sandbox_email" {
+  nullable = false
+  type     = string
+}
+
+variable "jawhite04_dns_manager_email" {
+  nullable = false
+  type     = string
+}
+
+variable "com_jawhite04_email" {
+  nullable = false
+  type     = string
+}
